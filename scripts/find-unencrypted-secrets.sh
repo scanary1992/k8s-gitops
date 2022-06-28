4ba245eea794f01ca29849cac749e9931cffcd9a
scanary <vlad_jan1992@mail.ru>
2022-06-28 22:36:32 +0300

    improved the script for searching for unencrypted secrets

--- scripts/find-unencrypted-secrets.sh
+++ scripts/find-unencrypted-secrets.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-FILE_WITH_SECRETS=$(grep -lZRPi '^kind:\s+secret' . | xargs -r0 grep -L 'ENC.AES256')
+FILE_WITH_SECRETS=$(grep -lZRPi '^kind:\s+secret' . | xargs -r0 grep -L -e 'ENC.AES256' -e 'ENC.ED25519')
 if [ -z "$FILE_WITH_SECRETS" ]; then
   exit 0
 else
-  echo "$FILE_WITH_SECRETS has unencrypted secrets! (or at least not encrypted with AES256)"
+  echo "$FILE_WITH_SECRETS has unencrypted secrets! (or at least not encrypted with AES256 or Ed25519)"
   exit 1
 fi
